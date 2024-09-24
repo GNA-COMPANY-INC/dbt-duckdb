ae6e04c821a1a20bb063d9fa29e11e4476d1453d
selene <selene.park@gna.company>
2024-09-24 15:04:04 +0900

    update to create mysql table

--- dbt/adapters/duckdb/plugins/playio.py
+++ dbt/adapters/duckdb/plugins/playio.py
@@ -9,7 +9,9 @@ from ..utils import TargetConfig
 from . import pd_utils
 import traceback
 import os
-
+import pandas
+import datetime
+from typing import List
 # ======== GNA-COMPANY custom plugin for dbt-duckdb ========
 # - made to supports multi-source read and multi-target write
 
@@ -18,13 +20,37 @@ import os
 #   - init fork: https://github.com/GNA-COMPANY-INC/dbt-duckdb
 #   - support deltalake table from minio: both read and write
 
-# - 2024-09-23 update
+# - 2024-09-22 update
 #   - support mysql table: read and write
 #   - support local file: write
 #   - tested dbt-python-model functionality
 
+# - 2024-09-24 update
+#   - create table if not exists for rds
+#   - support unique_cols and index_cols for rds
+
 class Plugin(BasePlugin):
     wp = WriterProperties(compression="ZSTD", compression_level=9)
+    
+    __rds_default_cols = [
+        "id BIGINT PRIMARY KEY AUTO_INCREMENT not null",
+        "created TIMESTAMP DEFAULT CURRENT_TIMESTAMP null",
+        "updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP null",
+    ]
+    
+    __rds_default_dtypes = {
+        'int64': 'BIGINT',
+        'float64': 'DOUBLE',
+        'object': 'VARCHAR(512)',
+        'datetime64[ns]': 'DATETIME',
+        'datetime64[ns, UTC]': 'DATETIME',
+        'timedelta64[ns]': 'TIME',
+        'bool': 'BOOLEAN',
+        'datetime.date': 'DATE',
+        'datetime.datetime': 'DATETIME'
+    }
+    
+    
     
     def initialize(self, plugin_config: Dict[str, Any])->None:
         self.plugin_config = plugin_config
@@ -76,9 +102,83 @@ class Plugin(BasePlugin):
             f"ATTACH 'host={h} user={u} port={p} password={pwd} database={scheme}' AS {db_alias} (TYPE MYSQL);"
         )
         self.conn.execute(f"USE {db_alias};")   
-        print(f"connected to rds: table: {table_name}")
+        print(f"connected to rds: {table_name}.{scheme}")
         return db_alias
+    
+    
+    def __create_table_if_needed(self, scheme:str, table_name:str, db_alias:str, df:pandas.DataFrame, uniq_cols:List[str]|str=None, index_cols:List[str]|str=None)->bool:
         
+        info_q = f"""
+                select TABLE_NAME 
+                from INFORMATION_SCHEMA.TABLES 
+                where TABLE_SCHEMA = ''{scheme}'' and TABLE_NAME = ''{table_name}'';            
+        """
+        info_q = f"""
+            select * from mysql_query('{db_alias}', '{info_q}');
+        """
+        
+        
+        if len(self.conn.execute(info_q).fetchall()) == 0:
+            print(f"table {scheme}.{table_name} does not exist, creating...")
+            ddl_cols = self.__rds_default_cols
+            for column, dtype in zip(df.columns, df.dtypes):
+                if pandas.api.types.is_datetime64_any_dtype(dtype):
+                    mysql_type = self.__rds_default_dtypes['datetime64[ns]']
+                elif isinstance(df[column].iloc[0], datetime.date):
+                    mysql_type = self.__rds_default_dtypes['datetime.date']
+                elif isinstance(df[column].iloc[0], datetime.datetime):
+                    mysql_type = self.__rds_default_dtypes['datetime.datetime']
+                else:
+                    max_data_len = df[column].apply(lambda x: len(str(x))).max() * 2
+                    
+                    mysql_type = f'VARCHAR({str(max_data_len)})'
+
+                _col_ddl = f"  {column} {mysql_type}"
+                ddl_cols.append(_col_ddl)
+            
+            
+            if uniq_cols is not None:
+                if isinstance(uniq_cols, str):
+                    uniq_cols = [uniq_cols]
+                _unq_ddl = f"constraint uq_{table_name}_{'_'.join(uniq_cols)} unique ({', '.join(uniq_cols)})"
+                ddl_cols.append(_unq_ddl)
+                
+            ddl_cols_str = ",\n".join(ddl_cols)
+            create_q = f"""
+                CREATE TABLE {scheme}.{table_name} (
+                    {ddl_cols_str}
+                ); 
+            """
+            print(f"create ddl: {create_q}")
+            create_q = f"""
+                CALL mysql_execute('{db_alias}', '{create_q}');
+            """
+            self.conn.execute(create_q)
+            
+            
+            index_ddl = [
+                f"create index idx_created on {scheme}.{table_name} (created);",
+                f"create index idx_updated on {scheme}.{table_name} (updated);",   
+            ]
+            
+            if index_cols is not None:
+                if isinstance(index_cols, str):
+                    index_cols = [index_cols]
+                for idx in index_cols:
+                    _ddl = f"create index idx_{idx} on {scheme}.{table_name} ({idx});"
+                    index_ddl.append(_ddl)
+                
+            index_ddl_str = "\n".join(index_ddl)
+            index_q = f"""
+                CALL mysql_execute('{db_alias}', '{index_ddl_str}'); 
+            """
+            self.conn.execute(index_q)
+            print(f"created table: {scheme}.{table_name}")
+            return True
+        else:
+            print(f"table {scheme}.{table_name} already exists")
+            return False
+                    
         
     def load(self, source_config: SourceConfig)->Any:
         print(f"loading source_config: {source_config}")
@@ -163,14 +263,22 @@ class Plugin(BasePlugin):
                 raise Exception("table and scheme are required arguments for rds")
             storage_options = self.__rds_conn_opt(region, storage)
             db_alias = self.__attach_to_rds(storage_options, scheme, table_name)
+            
             print(f"storing to rds table: {region}.{storage} {scheme}.{table_name}")
+            
             df = pd_utils.target_to_df(target_config)
             print(f"df:\n{df}")
+            
+            uniq_cols = target_config.config.get("unique_cols", None)
+            index_cols = target_config.config.get("index_cols", None)
+            self.__create_table_if_needed(scheme, table_name, db_alias, df, uniq_cols, index_cols)
+            
             cols = df.columns
             col_str = ", ".join(cols)
+            
             vals = []
             for _,row in df.iterrows():
-                vals_str = ", ".join([f"''{v}''" for v in row])
+                vals_str = ", ".join([f"NULL" if v is None else f"''{v}''" for v in row])
                 vals.append(f"({vals_str})")
             
             q = f"""
@@ -198,7 +306,7 @@ class Plugin(BasePlugin):
             path = os.path.join(root, path)
             if path.endswith(".csv"):
                 if os.path.exists(path):
-                    os.remove(path)                
+                    os.remove(path)
                 df.to_csv(path, index=False)
             elif path.endswith(".parquet"):
                 if os.path.exists(path):
@@ -207,10 +315,12 @@ class Plugin(BasePlugin):
             else:
                 raise Exception(f"Unsupported format: {path}")
             print(f"saved to local path: {path}")
+            
         elif storage_type == "ref":
             df = pd_utils.target_to_df(target_config)
             print(f"df:\n{df}")
             pass 
+        
         else:
             raise NotImplementedError("unsupported storage type")
         
